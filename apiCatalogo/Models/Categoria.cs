f06944a13bdf9c88304fd9fca9d9e2282a42a2eb
Genildo Martins <genildovsm@gmail.com>
2025-05-14 01:29:54 -0300

    Configuração de data annotations no modelo de dados

--- apiCatalogo/Models/Categoria.cs
+++ apiCatalogo/Models/Categoria.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace apiCatalogo.Models;
 
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
-    public string? Nome { get; set;}
+
+    [Required]
+    [Display(Name ="Nome")]
+    [StringLength(80, ErrorMessage = "O campo {0} deve ter no máximo {1} cartacteres")]
+    public string? Nome { get; set; }
+
+    [Required]
+    [Display(Name ="URL da imagem")]
+    [StringLength(300, ErrorMessage = "O campo {0} deve ter no máximo {1} cartacteres")]
     public string? ImagemUrl { get; set; }
+
     public ICollection<Produto> Produtos{ get; set; } = [];
 }
--- apiCatalogo/Models/Produto.cs
+++ apiCatalogo/Models/Produto.cs
@@ -1,14 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace apiCatalogo.Models;
 
 public class Produto
 {
+    [Key]
     public int ProdutoId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(255)]
     public string? Descricao { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(10,2)")]
     public decimal Preco {get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+    
+    [Display(Name = "Quantidade em estoque")]
+    [Range(0, float.MaxValue)]
     public float Estoque { get; set; }
+    
+    [DataType(DataType.DateTime)]
     public DateTime DataCadastro {get; set; }
+    
     public int CategoriaId { get; set; }
+    
     public Categoria Categoria { get; set; } = new Categoria();
 }
