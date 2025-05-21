bdab95aa034db15a940822353efd4563ab9ab03b
Genildo Martins <genildovsm@gmail.com>
2025-05-21 02:11:38 -0300

    Refatoração prevenir referência nula

--- apiCatalogo/Controllers/CategoriasController.cs
+++ apiCatalogo/Controllers/CategoriasController.cs
@@ -4,6 +4,7 @@ using apiCatalogo.DTOs.Views;
 using apiCatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 
 namespace apiCatalogo.Controllers;
 
@@ -28,46 +29,48 @@ public class CategoriasController(ApiCatalogoDbContext context) : ControllerBase
     [HttpGet("Produtos")]
     [ProducesResponseType(typeof(Categoria), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<ActionResult<IEnumerable<Categoria>>> GetCategoriasProdutos()
+    public async Task<IActionResult> GetCategoriasProdutos()
     {
-        var categorias = await _context.Categorias
+        IEnumerable categorias = await _context.Categorias
             .AsNoTracking()
             .Include(c => c.Produtos)
             .ToListAsync();
 
-        return categorias != null ? Ok(categorias) : NoContent();
+        return (categorias is null) ? NoContent() : Ok(categorias);
     }
 
     /// <summary>
-    /// Obtém uma categoria com base no id informado
+    /// Obtém a categoria por id
     /// </summary>
     /// <param name="id">Identificador da categoria</param>
     /// <returns>Retorna uma categoria</returns>
     /// <response code="200">Categoria encontrada</response>
     /// <response code="404">Categoria não encontrada</response>
-    [HttpGet("{id:int}", Name = "ObterCategoria")]
-    public async Task<ActionResult<CategoriaViewModel>> Get(int id)
+    [HttpGet("{id:int:min(1)}", Name = "ObterCategoria")]
+    [ProducesResponseType(typeof(Categoria), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get(int id)
     {
         Categoria? categoria = await _context.Categorias
             .Where(p => p.Id == id)
             .AsNoTracking()
             .FirstOrDefaultAsync();
 
-        if (categoria == null) return NotFound("Categoria não encontrada");
+        if (categoria is null) return NotFound("Categoria não encontrada");
 
-        return new CategoriaViewModel(categoria); 
+        return Ok((CategoriaViewModel)categoria); 
     }
 
     /// <summary>
     /// Cria um nova categoria
     /// </summary>
-    /// <param name="categoria">Instãncia da entidade categoria</param>
+    /// <param name="model">Modelo de entrada para categoria</param>
     /// <returns>Retorna a categoria adicionada</returns>
     /// <response code="400">Categoria não informada</response>
     [HttpPost]
-    public async Task<ActionResult<Categoria>> Post(Categoria categoria)
+    public async Task<IActionResult> Post(CategoriaInputModel model)
     {
-        if (categoria is null) return BadRequest("");
+        Categoria categoria = model;
 
         _context.Categorias.Add(categoria);
         await _context.SaveChangesAsync();
@@ -83,11 +86,9 @@ public class CategoriasController(ApiCatalogoDbContext context) : ControllerBase
     /// <returns>Retorna uma categoria atualizada</returns>
     /// <response code="400">Categoria não informada</response>
     /// <response code="200">Categoria atualizada</response>
-    [HttpPut("{id:int}")]
-    public async Task<ActionResult<Categoria>> Put(int id, CategoriaInputModel model)
+    [HttpPut("{id:int:min(1)}")]
+    public async Task<IActionResult> Put(int id, CategoriaInputModel model)
     {
-        if (model is null) return BadRequest("Categoria não informada");
-
         Categoria categoria = model;
         categoria.Id = id;
 
@@ -103,14 +104,14 @@ public class CategoriasController(ApiCatalogoDbContext context) : ControllerBase
     /// <param name="id">identificador da categoria</param>
     /// <returns></returns>
     /// <response code="204">Retorna a categoria excluída</response>
-    /// <response code="400">Categoria não localizada</response>
-    [HttpDelete("{id:int}")]
-    public async Task<ActionResult<Categoria>> Delete(int id)
+    /// <response code="404">Categoria não localizada</response>
+    [HttpDelete("{id:int:min(1)}")]
+    public async Task<IActionResult> Delete(int id)
     {
         var categoria = await _context.Categorias
             .FirstOrDefaultAsync(c => c.Id == id);
 
-        if (categoria is null) return BadRequest("Categoria não encontrada");
+        if (categoria is null) return NotFound("Categoria não encontrada");
 
         _context.Remove(categoria);
         await _context.SaveChangesAsync();
--- apiCatalogo/DTOs/Inputs/CategoriaInputModel.cs
+++ apiCatalogo/DTOs/Inputs/CategoriaInputModel.cs
@@ -1,4 +1,4 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 using apiCatalogo.Models;
 
 namespace apiCatalogo.DTOs.Inputs;
@@ -11,11 +11,15 @@ public class CategoriaInputModel
     /// <summary>
     /// Nome da categoria
     /// </summary>
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "{0}: Campo obrigatório")]
     public string Nome { get; set; } = string.Empty;
 
     /// <summary>
     /// URL da imagem da categoria
     /// </summary>
+    [Display(Name = "URL da imagem")]
+    [Required(ErrorMessage = "{0}: Campo obrigatório")]
     public string ImagemUrl { get; set; } = string.Empty;
 
     /// <summary>
